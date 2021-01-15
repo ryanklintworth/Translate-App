0e22ea9b672aa416ccde943be54f3ca3af067e6c
ryanklintworth <ryan.klintworth@gmail.com>
2021-01-14 19:32:02 -0600

    server.js file setup

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+// ================
+// middlewere
+// ================
+
+app.use(express.json())
+app.use(express.static('public'))
+
+
+// ================
+// controllers
+// ================
+
+const moviesController = require('./controllers/items_controller.js')
+app.use('/items', moviesController)
+
+// ================
+// database
+// ================
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+mongoose.connection.on('error', err =>
+  console.log(
+    err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+  )
+)
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: 🧜‍♂️', MONGODB_URI)
+)
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+app.listen(PORT, () => {
+ console.log(`listening on port ${PORT}‍ 🍭`);
+})
