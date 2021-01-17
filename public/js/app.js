0ac078c11c1a9be477300c4cf18842196f97733f
ryanklintworth <ryan.klintworth@gmail.com>
2021-01-17 13:00:09 -0600

    updated the close modal function to the create button

--- public/js/app.js
+++ public/js/app.js
@@ -35,7 +35,7 @@ class Garage extends React.Component {
     .delete('/items/' + event.target.value)
     .then((response) => {
       this.setState({
-        podcasts: response.data,
+        items: response.data,
       })
     })
   }
@@ -75,56 +75,31 @@ class Garage extends React.Component {
 
   render = () => {
     return( <div className="main">
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Post</a></li>
-          <li><a href="#">Catergories</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#">Sign Up</a></li>
-          </ul>
-        </div>
-      </nav>
-      <br/><br/>
-        <div id="modal">
-          <div id="modal-textbox">
-            <form onSubmit={this.handleSubmit}>
-              <label htmlFor="name"><p className="inputText">Name</p></label>
-                <input type="text" className="textbox" onChange={this.handleChange} />
-              <label htmlFor="image"><p className="inputText">Image</p></label>
-                <input type="text" className="textbox" onChange={this.handleChange} />
-              <label htmlFor="description"><p className="inputText">description</p></label>
-                <input type="text" className="textbox" onChange={this.handleChange} />
-              <label htmlFor="price"><p className="inputText">Price</p></label>
-                <input type="text" className="textbox" onChange={this.handleChange} />
-              <label htmlFor="location"><p className="inputText">Location</p></label>
-                <input type="text" className="textbox" onChange={this.handleChange} />
-                <br/><br/>
-                <input id="updatebtn" onClick={this.closeModal} type="submit" value="Create" />
-            </form>
-            <br/>
-            <button id="closeModal" onClick={this.closeModal}>Cancel</button>
-          </div>
-        </div>
-        <button className="btn waves-effect waves-light" onClick={this.openModal}> Post an item for sale </button>
-          <h2 id="list">Items for sale</h2>
-            <div id="itemContainer">
-              <ul>
-                {this.state.items.map((items) => {
-                  return(
-                    <li>
-                    <p id="itemName">{items.name}</p>
-                    <br/>
-                    <p id="itemDesc">{items.description}</p>
-                    <br/>
-                    <img src={items.image} alt={items.name} />
-                    <br/>
-                    <p id="itemPrice">{items.price}</p>
-                    <br/>
-                    <p id="itemLoc">{items.location}</p>
+          <nav id="navbar">
+            <a href="#">Home</a>
+            <a href="#">Post</a>
+            <a href="#">Catergories</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+            <a href="#">Sign Up</a>
+          </nav>
+          <br/><br/>
+                <div id="modal">
+                  <div id="modal-textbox">
+                    <form onSubmit={this.handleSubmit}>
+                      <label htmlFor="name"><p className="inputText">Name</p></label>
+                        <input type="text" id="name" className="textbox" onChange={this.handleChange} />
+                      <label htmlFor="image"><p className="inputText">Image</p></label>
+                        <input type="text" id="image" className="textbox" onChange={this.handleChange} />
+                      <label htmlFor="description"><p className="inputText">description</p></label>
+                        <input type="text" id="description" className="textbox" onChange={this.handleChange} />
+                      <label htmlFor="price"><p className="inputText">Price</p></label>
+                        <input type="text" id="price" className="textbox" onChange={this.handleChange} />
+                      <label htmlFor="location"><p className="inputText">Location</p></label>
+                        <input type="text" id="location" className="textbox" onChange={this.handleChange} />
+                        <br/><br/>
+                        <input type="submit" onClick={this.closeModal} id="updatebtn" value="Create" />
+                    </form>
                     <br/>
                     <button id="closeModal" onClick={this.closeModal}>Cancel</button>
                   </div>
